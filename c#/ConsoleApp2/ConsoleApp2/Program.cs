fb04ec1801ea676baf95f9d8d84defb84cbf8534
Gaoming Lin <gaominglins@gmail.com>
2022-05-31 16:46:16 -0700

    abb

--- c#/ConsoleApp2/ConsoleApp2/Program.cs
+++ c#/ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Why OOP?
+//write clean code; defend our code against errors; build API or .NET Application
+
+// Class: blueprint implemented by code
+// class contains properties and methods.
+// properties: data that contained in a class
+// methods: operations on a class
