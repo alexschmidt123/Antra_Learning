7384bfb4d04b49ceaf17382ab57a2cc55fa7a47d
Gaoming Lin <gaominglins@gmail.com>
2022-05-30 21:50:32 -0700

    jhvihv

--- c#/ConsoleApp1/ConsoleApp1/Program.cs
+++ c#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp1;
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] arg)
+        {
+            ParamPassing demo = new ParamPassing();
+            string str;
+            Console.WriteLine(demo.IsAuthentic("rebecca", "liu", out str));
+            Console.WriteLine(str);
+        }
+    }
+
+}
