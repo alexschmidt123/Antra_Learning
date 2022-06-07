6b453b312589eb0f7f1c8f17ee153bffb7679499
Gaoming Lin <gaominglins@gmail.com>
2022-06-07 14:18:47 -0400

    ioho

---
+++ c#/ConsoleApp6/ConsoleApp6.Core/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6.Core.Entities
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DName { get; set; }
+        public string Loc { get; set; }
+
+    }
+}
