06f3ad52201d34ce433c5ac98819844f2c3c05a3
Gaoming Lin <gaominglins@gmail.com>
2022-06-10 15:42:37 -0400

    fvr

---
+++ frontEnd/day4/demo.js
@@ -0,0 +1,8 @@
+//local storage vs. session storage
+
+
+//btoa vs. atob
+const encodedData = btoa('Hello, world'); // encode a string
+const decodedData = atob(encodedData); // decode the string
+console.log(encodedData);
+console.log(decodedData);
