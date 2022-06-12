8f8447c209b66812d99fb95a7e536b58befcb029
Gaoming Lin <gaominglins@gmail.com>
2022-06-12 14:38:24 -0400

    eqcqef

--- frontEnd/day4/problems.js
+++ frontEnd/day4/problems.js
@@ -1,30 +1,47 @@
 // 1. 
 // We have an object storing salaries of our team
 let salaries = {
-John: 100,
-Ann: 160,
-Pete: 130
+    John: 100,
+    Ann: 160,
+    Pete: 130
 }
 // Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
-
+function SumUp(salaries) {
+    var sum = 0;
+    for (const key in salaries) {
+        sum += salaries[key];
+    }
+    return sum;
+}
+console.log(SumUp(salaries));
 
 // 2.
 // Create a function multiplyNumeric(obj) that multiplies all numeric properties of obj by 2
 // before the call
 let
-menu = {
-width: 200,
-height: 300,
-title: 
-"My menu"
-};
+    menu = {
+        width: 200,
+        height: 300,
+        title:
+            "My menu"
+    };
+
+function multiplyNumeric(menu) {
+    for (const key in menu) {
+        if (typeof menu[key] == "number") {
+            menu[key] = 2 * menu[key];
+        }
+    }
+}
+
 multiplyNumeric(menu);
+console.log(menu);
 // after the call
 menu = {
-width: 400,
-height: 600,
-title: 
-"My menu"
+    width: 400,
+    height: 600,
+    title:
+        "My menu"
 };
 // Please note that multiplyNumeric does not need to return anything. It should modify the object in place
 
@@ -32,7 +49,21 @@ title:
 // 3.
 // Write a function checkEmailId(str) that returns true if str contains '@' and ‘.’, otherwise false. Make sure '@' must come before '.' and there must be some characters between '@' and '.'
 // The function must be case-insensitive:
-
+var inputText = "aklex@vcr.com";
+function ValidateEmail(inputText) {
+    var mailFormat =/^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/;
+    if (inputText.value.match(mailFormat)) {
+        alert("Valid email address!");
+        document.myForm.Email.focus();
+        return true;
+    }
+    else {
+        alert("You have entered an invalid email address!");
+        document.myForm.Email.focus();
+        return false;
+    }
+}
+console.log(ValidateEmail(inputText));
 
 // 4.
 // Create a function truncate(str, maxlength) that checks the length of the str and, if it exceeds maxlength –replaces the end of str with the ellipsis character "...", to make its length equal to maxlength.
