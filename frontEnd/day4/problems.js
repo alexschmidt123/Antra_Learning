b5af157ded48bedc2d4f30134434488b3cc7bfd5
Gaoming Lin <gaominglins@gmail.com>
2022-06-12 15:20:10 -0400

    2ecve

--- frontEnd/day4/problems.js
+++ frontEnd/day4/problems.js
@@ -18,8 +18,7 @@ console.log(SumUp(salaries));
 // 2.
 // Create a function multiplyNumeric(obj) that multiplies all numeric properties of obj by 2
 // before the call
-let
-    menu = {
+let menu = {
         width: 200,
         height: 300,
         title:
@@ -37,39 +36,55 @@ function multiplyNumeric(menu) {
 multiplyNumeric(menu);
 console.log(menu);
 // after the call
-menu = {
-    width: 400,
-    height: 600,
-    title:
-        "My menu"
-};
+// menu = {
+//     width: 400,
+//     height: 600,
+//     title:
+//         "My menu"
+// };
 // Please note that multiplyNumeric does not need to return anything. It should modify the object in place
 
 
 // 3.
 // Write a function checkEmailId(str) that returns true if str contains '@' and ‘.’, otherwise false. Make sure '@' must come before '.' and there must be some characters between '@' and '.'
 // The function must be case-insensitive:
-var inputText = "aklex@vcr.com";
+let inputText={
+    value: 'alex@ceec.ceom',
+}
 function ValidateEmail(inputText) {
     var mailFormat =/^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/;
-    if (inputText.value.match(mailFormat)) {
-        alert("Valid email address!");
-        document.myForm.Email.focus();
-        return true;
-    }
-    else {
-        alert("You have entered an invalid email address!");
-        document.myForm.Email.focus();
+    if (!inputText.value.match(mailFormat)) {
         return false;
     }
+    else {
+        return true;
+    }
 }
+console.log(typeof inputText.value);
 console.log(ValidateEmail(inputText));
+var mailFormat =/^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/;
+var str=" fvcf";
+var str1 = "cwvc@ceec.com";
+const rvc  = str.match(mailFormat);
+console.log(str.match(mailFormat));
+console.log(str1.match(mailFormat));
+
 
 // 4.
 // Create a function truncate(str, maxlength) that checks the length of the str and, if it exceeds maxlength –replaces the end of str with the ellipsis character "...", to make its length equal to maxlength.
 // The result of the function should be the truncated (if needed) string.
-truncate("What I'd like to tell on this topic is:", 20) = "What I'd like to te..."
-truncate("Hi everyone!", 20) = "Hi everyone!"
+// truncate("What I'd like to tell on this topic is:", 20) = "What I'd like to te..."
+// truncate("Hi everyone!", 20) = "Hi everyone!"
+function truncate(str, num){
+    if (str.length<num){
+        return str;
+    }else{
+        return str.split("", num-1).join("") + "...";
+    }
+}
+console.log(truncate("What I'd like to tell on this topic is:", 20));
+console.log(truncate("Hi everyone!", 20));
+
 
 
 // 5.
@@ -79,8 +94,19 @@ truncate("Hi everyone!", 20) = "Hi everyone!"
 // Replace the value in the middle by “Calvin”. Your code for finding the middle value should work for any arrays with odd length.
 // Remove the first value of the array and show it.
 // Prepend Rose and Regal to the array.
-James, Brennie
-James, Brennie, Robert
-James, Calvin, Robert
-Calvin, Robert
-Rose, Regal, Calvin, Robert
+// James, Brennie
+// James, Brennie, Robert
+// James, Calvin, Robert
+// Calvin, Robert
+// Rose, Regal, Calvin, Robert
+
+const array = ["James", "Brennie"];
+console.log(array);
+array.push("Robert");
+console.log(array);
+array[Math.floor(array.length/2)]="Calvin"; //array.length should be odd
+console.log(array);
+array.splice(0, 1);
+console.log(array);
+array.splice(0,0,"Rose","Regal");
+console.log(array);
