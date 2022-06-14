ed63a95d630e2991eb460680efa424cb82408f15
Gaoming Lin <gaominglins@gmail.com>
2022-06-13 21:00:11 -0400

    4vc453

---
+++ asp/MovieShop/MovieShopMVC/Controllers/MoviesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MovieShopMVC.Models;
+
+namespace MovieShopMVC.Controllers
+{
+    public class MoviesController: Controller
+    {
+        private readonly ILogger<MoviesController> _logger;
+
+        public MoviesController(ILogger<MoviesController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
