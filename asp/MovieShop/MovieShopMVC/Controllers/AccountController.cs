9322d8ff1c5643906c3a1b8210cb296348987268
Gaoming Lin <gaominglins@gmail.com>
2022-06-29 14:42:32 -0400

    dfhjn

--- asp/MovieShop/MovieShopMVC/Controllers/AccountController.cs
+++ asp/MovieShop/MovieShopMVC/Controllers/AccountController.cs
@@ -1,10 +1,35 @@
-﻿using System.Diagnostics;
+﻿using ApplicationCore.Models;
 using Microsoft.AspNetCore.Mvc;
-using MovieShopMVC.Models;
 
 namespace MovieShopMVC.Controllers
 {
-    public class AccountController: Controller
+    public class AccountController : Controller
     {
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(UserLoginModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            // show the empty register Page when we make a GET Request
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterModel model)
+        {
+            //model binding
+            return View();
+        }
+
     }
 }
