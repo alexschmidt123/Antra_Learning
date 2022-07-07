415da08ef691e50056e022b6949ab21e95adcb1b
Gaoming Lin <gaominglins@gmail.com>
2022-07-07 01:57:00 -0400

    t4btn

--- angular/MovieShopSPA/src/app/Public/genre/genre-routing.module.ts
+++ angular/MovieShopSPA/src/app/Public/genre/genre-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddGenreComponent } from './add-genre/add-genre.component';
 import { GenreComponent } from './genre.component';
+import { MoviesByGenreComponent } from './movies-by-genre/movies-by-genre.component';
 
 const routes: Routes = [
   {path: '', component: GenreComponent}, 
-  {path: 'addGenre', component: AddGenreComponent}  
+  {path: 'addGenre', component: AddGenreComponent},
+  {path: 'MovieByGenre', component: MoviesByGenreComponent}  
 ];
 
 @NgModule({
--- angular/MovieShopSPA/src/app/Public/movies.module.ts
+++ angular/MovieShopSPA/src/app/Public/movies.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { MoviesRoutingModule } from './movies-routing.module';
 import { HomeComponent } from './home/home.component';
 import { MovieComponent } from './movie-details/movie.component';
 import { ReviewsComponent } from './reviews/reviews.component';
+import { GenreModule } from './genre/genre.module';
 
 
 @NgModule({
@@ -15,7 +15,8 @@ import { ReviewsComponent } from './reviews/reviews.component';
   ],
   imports: [
     CommonModule,
-    MoviesRoutingModule
+    MoviesRoutingModule,
+    GenreModule
   ]
 })
 export class MoviesModule { }
