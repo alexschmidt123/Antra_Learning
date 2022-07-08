d3a24fa9f1d5b46a059d17a8ae52c3ca4dfeb7d1
Gaoming Lin <gaominglins@gmail.com>
2022-07-07 23:14:07 -0400

    hnrds

--- angular/MovieShopSPA/src/app/Account/register/register.component.ts
+++ angular/MovieShopSPA/src/app/Account/register/register.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, NgForm } from '@angular/forms';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { RegisterValidationServiceService } from 'src/app/Core/Custom-Validators/register-validation-service.service';
+import { AccountService } from 'src/app/Core/Services/account.service';
+import { Register } from 'src/app/Shared/Models/register';
 
 @Component({
   selector: 'app-register',
@@ -9,18 +12,20 @@ import { RegisterValidationServiceService } from 'src/app/Core/Custom-Validators
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  //registration:Register;
+
+  submitted:boolean = false;
+  failed:boolean = false;
   registerForm: FormGroup;
-  submitted:boolean=false;
-  constructor(private fb:FormBuilder, private customValidator:RegisterValidationServiceService) { }
+  registered:boolean=false;
+  constructor(private fb:FormBuilder, private customValidator:RegisterValidationServiceService, private registerService:AccountService, private _router:Router) { }
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.compose([Validators.required, this.customValidator.patternValidator()])],
       confirmPassword: ['', Validators.required],
+      lastName: ['', Validators.required],
+      firstName: ['', Validators.required],
       dateOfBirth: ['', Validators.required],
     },
     {
@@ -35,9 +40,26 @@ export class RegisterComponent implements OnInit {
   onSubmit(){
     this.submitted = true;
     if (this.registerForm.valid){
-      alert('Form Submitted Successfully!');
       console.table(this.registerForm.value);
-      //this.registerService.Register(this.registerForm);
+      
+      const registration:Register = {
+      email: this.registerForm.controls['email'].value,
+      password: this.registerForm.controls['password'].value,
+      lastName: this.registerForm.controls['lastName'].value,
+      firstName: this.registerForm.controls['firstName'].value,
+      dateOfBirth: this.registerForm.controls['dateOfBirth'].value,
+      };
+      
+      this.registerService.Register(registration).subscribe(r => {
+        if (r){
+          this.registered = true;
+          setTimeout(() => {
+            this._router.navigateByUrl('/Account/Login');
+          }, 10000);
+        } else {
+          this.failed = true;
+        };
+      })
     }
   }
 
--- angular/MovieShopSPA/src/app/Core/Services/account.service.ts
+++ angular/MovieShopSPA/src/app/Core/Services/account.service.ts
@@ -1,11 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Login } from 'src/app/Shared/Models/login';
+import { Register } from 'src/app/Shared/Models/register';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  constructor() { }
-  Login(){}
-  Logout(){}
+  constructor(private http:HttpClient) { }
+
+  Login(login:Login):Observable<boolean>{
+    return this.http.post('https://localhost:7194/api/Account/login', login).pipe(map((response:any) => {
+      if (response){
+        localStorage.setItem('token', response.token);
+        return true;
+      }
+      return false;
+    }))
+  }
+
+  Logout(){
+    localStorage.removeItem('token');
+  }
+
+  Register(registration:Register):Observable<boolean>{
+    return this.http.post<boolean>('https://localhost:7194/api/Account/register', registration);
+  }
+
 }
