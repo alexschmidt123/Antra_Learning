c461244236cb3a9d182846283ef6d7745880d3a8
Gaoming Lin <gaominglins@gmail.com>
2022-07-08 23:45:52 -0400

    tyjm

--- angular/MovieShopSPA/src/app/Core/Layout/header/header.component.ts
+++ angular/MovieShopSPA/src/app/Core/Layout/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/Shared/Models/user';
+import { AccountService } from '../../Services/account.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn:boolean = false;
+  currentUser:User;
+  constructor(private accountService:AccountService, private _router:Router) { }
 
   ngOnInit(): void {
+    this.accountService.isLoggedIn.subscribe(data => {
+      this.isLoggedIn = data;
+    });
+    this.accountService.currentUser.subscribe(data => {
+      this.currentUser = data;
+    });
+  }
+
+  Logout(){
+    this.accountService.Logout();
+    this._router.navigateByUrl('/');
   }
 
 }
