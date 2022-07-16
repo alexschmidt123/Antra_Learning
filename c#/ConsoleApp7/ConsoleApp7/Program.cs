e9b5fc4336ab942d0b02fe80f500fc8db0d2b2bb
Gaoming Lin <gaominglins@gmail.com>
2022-07-16 10:19:36 -0400

    dnnnnndy

---
+++ c#/ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,49 @@
+﻿
+namespace ConsoleApp7{
+
+    public class Rectangle
+    {
+        private int width;
+        private int height;
+
+        public void setWidth(int width)
+        {
+            this.width = width;
+        }
+        public void setHeight(int height)
+        {
+            this.height = height;
+        }
+        public int area() { return height * width; }
+    }
+
+    public class Square: Rectangle
+    {
+        public void setWidth(int width)
+        {
+            base.setWidth(width);
+            base.setHeight(width);
+        }
+        public void setHeight(int height)
+        {
+            base.setWidth(height);
+            base.setHeight(height);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            rect.setWidth(4);
+            rect.setHeight(5);
+            Console.WriteLine("Rectangle Area: "+rect.area());
+            Square squa = new Square();
+            squa.setWidth(4);
+            squa.setHeight(5);
+            Console.WriteLine("Square Area: " + squa.area());
+        }
+    }    
+}
+
